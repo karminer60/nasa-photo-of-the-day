77d68e59bbac22b6468f289e6d209b95451b6c7d
karina-rodriguez <karminer60@gmail.com>
2020-05-13 19:54:02 -0700

    trying to get API data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
-import "./App.css";
 
+//add state for the data you'll be getting from NASA.
+//Add an effect hook to handle the API call side effect.
+import React, { useState, useEffect } from 'react'
+import "./App.css";
+const [comments, setComments] = useState(props.comments); 
+
+useEffect(() => {
+  // this code in here will run, after the first DOM surgery only
+  // BASE_URL and API_KEY (http://localhost:4000/friends?api_key=xyz)
+  axios.get(`https://api.nasa.gov/planetary/apod?api_key=W57gnrIioT3Fpi94SzcBtWGEBvcRRmIoii2k1DqH`)
+    .then(response => {
+      response.data
+    })
+    .catch(err => {
+      debugger
+    })
+}, [])
 function App() {
   return (
     <div className="App">
